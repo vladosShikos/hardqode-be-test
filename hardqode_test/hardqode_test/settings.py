ae7472adb2a63c32874566f819dd9985e0bf8a48
vladosShikos <vladsyturinn@gmail.com>
2023-09-26 19:36:49 +0300

    api query via url filtering

--- hardqode_test/hardqode_test/settings.py
+++ hardqode_test/hardqode_test/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
     'elearningbe.apps.ElearningbeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -122,3 +123,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
