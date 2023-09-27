23447734a87680c1bc46bf70b5ee4dbabe4ac744
vladosShikos <vladsyturinn@gmail.com>
2023-09-27 21:24:56 +0300

    tinkering with agregate qsets

--- hardqode_test/elearningbe/serializers.py
+++ hardqode_test/elearningbe/serializers.py
@@ -44,11 +44,20 @@ class UserProductAvailableLessonsWithStatsSerializer(serializers.HyperlinkedMode
         fields = ['user', 'product', 'lesson', 'whatch_time', 'was_watched', 'last_watch_date']
 
 class ProductStatSerializer(serializers.HyperlinkedModelSerializer):
-    user = UserSerializer
-    Products = ProductSerializer    
-    lessons = LessonSerializer
-    lessons = LessonSerializer(many=True, read_only=True)
-    was_watched = serializers.BooleanField()
+    # user = UserSerializer
+    # Products = ProductSerializer    
+    # lessons = LessonSerializer
+    # lessons = LessonSerializer(many=True, read_only=True)
+    # was_watched = serializers.BooleanField()
+    # Количество просмотренных уроков от всех учеников.
+    total_lessons_watched = serializers.IntegerField()
+    # Сколько в сумме все ученики потратили времени на просмотр роликов.
+    total_watch_time = serializers.IntegerField()
+    # Количество учеников занимающихся на продукте.
+    total_number_students = serializers.IntegerField()
+    # Процент приобретения продукта (рассчитывается исходя из количества полученных доступов к продукту деленное на общее количество пользователей на платформе).
+    acquering_percent = serializers.IntegerField()
     class Meta:
         model = UserProductLessonHistory
-        fields = ['user', 'product', 'lessons', 'whatch_time', 'last_watch_date', 'was_watched']
+        # fields = ['user', 'product', 'lessons', 'whatch_time', 'last_watch_date', 'was_watched']
+        fields = ['total_lessons_watched', 'total_number_students', 'acquering_percent', 'total_watch_time']
--- hardqode_test/elearningbe/views.py
+++ hardqode_test/elearningbe/views.py
@@ -8,7 +8,7 @@ from .models import UserProductLessonHistory, User, Product, Lesson
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from django.db.models import FilteredRelation, Q, F
-from django.db.models import Count, Case, When, IntegerField, Value, ExpressionWrapper, BooleanField
+from django.db.models import Count, Case, When, IntegerField, Value, ExpressionWrapper, BooleanField, Sum
 
 class UserAvailableLessonsWithStats(generics.ListAPIView):
     serializer_class = UserAvailableLessonsWithStatsSerializer
@@ -40,8 +40,20 @@ class LessonViewSet(viewsets.ModelViewSet):
   
 class ProductStats(generics.ListAPIView):
     serializer_class = ProductStatSerializer
-    queryset = UserProductLessonHistory.objects.filter(id=1)
+    queryset = UserProductLessonHistory.objects.all()
     def get_queryset(self):
         product = self.kwargs['product']
+        #whatch_time field
         qs = UserProductLessonHistory.objects.filter(product__name=product).annotate(was_watched=ExpressionWrapper(Q(whatch_time__gte=F("lesson__length_in_seconds")*0.8), output_field=BooleanField()))
+        #total_lessons_watched field
+        # lessons_wathced = qs.aggregate(wathced_lessons=Count(Case(When(was_watched=True, then=Value(1)))))
+        lessons_wathced = -1
+        qs = qs.annotate(total_lessons_watched=Value(lessons_wathced))
+        #total_watch_time field
+        total_watch_time = UserProductLessonHistory.objects.filter(product__name=product).aggregate(Sum('whatch_time'))['whatch_time__sum']
+        qs = qs.annotate(total_watch_time=Value(total_watch_time))
+        # total_number_students field
+        qs = qs.annotate(total_number_students=Value(1))
+        # acquering_percent field
+        qs = qs.annotate(acquering_percent=Value(1))
         return qs
