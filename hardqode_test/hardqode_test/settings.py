4b80e3bda3b4648a50763c1d16a5aca394436dd8
vladosShikos <vladsyturinn@gmail.com>
2023-09-28 22:28:56 +0300

    add djagno-debug-toolbar

--- hardqode_test/hardqode_test/settings.py
+++ hardqode_test/hardqode_test/settings.py
@@ -31,8 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'debug_toolbar',
     'rest_framework',
-    'elearningbe.apps.ElearningbeConfig',
+    'elearning',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
