f43c76c26cf2d38492d16a03481154a0cc891d7b
vladosShikos <vladsyturinn@gmail.com>
2023-09-28 22:31:37 +0300

    remove admin models registretions

--- hardqode_test/elearning/admin.py
+++ hardqode_test/elearning/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(User)
-admin.site.register(Product)
-admin.site.register(ProductAccessToken)
-admin.site.register(Lesson)
-admin.site.register(UserProductLessonHistory)
+# admin.site.register(User)
+# admin.site.register(Product)
+# admin.site.register(ProductAccessToken)
+# admin.site.register(Lesson)
+# admin.site.register(UserProductLessonHistory)
 
 # Register your models here.
